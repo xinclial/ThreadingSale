4639431cbf94327a28d25ccc3595c71c124e1156
程伟 <chengwei10@centaline.com.cn>
2017-07-12 11:53:33 +0800

    update

---
+++ AsyncAndAwait/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAndAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Method1();
+            Method2();
+            Console.WriteLine("Main Thread");
+            Console.ReadLine();
+        }
+
+        private static void Method1()
+        {
+            Task.Run(new Action(LongTask));
+            Console.WriteLine("新线程");
+        }
+
+        private static async void Method2()
+        {
+            //await 等待执行完成后,在执行后续(必须用在异步中)
+            await Task.Run(new Action(LongTask));
+            Console.WriteLine("新线程");
+        }
+
+        private static void LongTask()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("线程中");
+        }
+
+
+        
+    }
+}
